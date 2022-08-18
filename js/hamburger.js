7e61ba48d9e289ac7c8b43a6657627d23f21fedb
Kybik <82117054+kybikn@users.noreply.github.com>
2022-08-18 13:34:15 +0300

    feat: add burger shadow

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,14 @@
+const shadow = document.querySelector(".shadow");
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".header__nav");
 const welcomeContent = document.querySelector(".welcome__content");
 // const headerLink = document.querySelector(".header__link");
+// const body = document.querySelector("body");
 
 hamburger.addEventListener("click", toggleMenu);
 nav.addEventListener("click", closeMenu);
 welcomeContent.addEventListener("click", closeMenu);
+shadow.addEventListener("click", closeMenu);
 // headerLink.addEventListener("click", closeMenu);
 
 function toggleMenu() {
@@ -14,14 +17,21 @@ function toggleMenu() {
   hamburger.classList.toggle("change");
   welcomeContent.classList.toggle("active");
   // headerLink.classList.toggle("active");
+  shadow.classList.toggle("active");
+  body.classList.toggle("active");
 }
 
 function closeMenu(event) {
-  if (event.target.classList.contains("header__link")) {
+  if (
+    event.target.classList.contains("header__link") ||
+    event.target.classList.contains("shadow")
+  ) {
     nav.classList.remove("navbar");
     hamburger.classList.remove("change");
     welcomeContent.classList.remove("active");
     // headerLink.classList.remove("active");
+    shadow.classList.remove("active");
+    body.classList.remove("active");
   }
 }
 
--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,38 @@
+const shadow = document.querySelector(".shadowModal");
 const modal = document.querySelector(".modal__block");
 modal.addEventListener("click", toggleModal);
+shadow.addEventListener("click", toggleModal);
 
 function toggleModal(event) {
   event.preventDefault();
-  if (event.target.classList.contains("close")) {
-    modal.classList.remove("active-modal");
-    body.classList.remove("active");
+  if (
+    event.target.classList.contains("close") ||
+    event.target.classList.contains("close-all") ||
+    event.target.classList.contains("shadowModal") ||
+    event.target.classList.contains("modal__block")
+  ) {
+    modal.classList.remove("active");
+    // modal.classList.remove("active-modal");
+    shadow.classList.remove("active");
   } else {
-    modal.classList.add("active-modal");
-    body.classList.add("active");
+    modal.classList.toggle("active");
+    // modal.classList.toggle("active-modal");
+    shadow.classList.toggle("active");
   }
 }
+
+// function toggleModal() {
+//   shadow.classList.toggle("active");
+//   modal.classList.toggle("active");
+// }
+
+// function closeModal(event) {
+//   if (
+//     event.target.classList.contains("close") ||
+//     event.target.classList.contains("close-all") ||
+//     event.target.classList.contains("shadow")
+//   ) {
+//     shadow.classList.remove("active");
+//     modal.classList.remove("active");
+//   }
+// }
